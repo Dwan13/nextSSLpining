c49565a59c737c72c651314b6ed1ec8cf26b541b
Dwan Veloza <dwanpaez@gmail.com>
2024-11-07 01:00:12 -0500

    ajuste redis

--- src/utils/auth/redis.ts
+++ src/utils/auth/redis.ts
@@ -4,11 +4,7 @@ import fs from 'fs';
 const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
-  tls: {
-    rejectUnauthorized: true,
-    ca: [fs.readFileSync('./certificates/ca-cert.pem')],
-  }
+  password: process.env.REDIS_PASSWORD || undefined
 });
 
 export default redis;
