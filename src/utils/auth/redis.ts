8e4c85da25bab5cc132a85e7c9cf1fabba938d14
Dwan Veloza <dwanpaez@gmail.com>
2024-11-07 19:01:52 -0500

    agregando cetificates

--- src/utils/auth/redis.ts
+++ src/utils/auth/redis.ts
@@ -1,10 +1,12 @@
 import Redis from 'ioredis';
-import fs from 'fs';
 
 const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD || undefined
+  password: process.env.REDIS_PASSWORD || undefined,
+  tls:{
+    ca: '/certificatesLocale/ca-cert.pem'
+  }
 });
 
 export default redis;
